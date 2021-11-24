ed1e9fbd3978bd2af0d6d20a69203d907e684383
Ray Deck <ray@raydeck.com>
2021-11-24 17:54:40 -0500

    feat: initial commit

--- App.tsx
+++ App.tsx
@@ -1,11 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StatusBar } from "expo-status-bar";
+import React, { useEffect } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import {
+  useLastNotificationResponse,
+  usePermissions,
+  getExpoPushTokenAsync,
+} from "expo-notifications";
 export default function App() {
+  const notificationStatus = usePermissions({ request: true, get: true });
+  const lastnotification = useLastNotificationResponse();
+  useEffect(() => {
+    (async () => {
+      if (notificationStatus[0]?.granted) {
+        const token = await getExpoPushTokenAsync({
+          experienceId: "@raydeck/notitest",
+        });
+        console.log("I got a token", token);
+      } else {
+        console.log("I don't have permission");
+      }
+    })();
+  }, [notificationStatus]);
+  useEffect(() => {
+    console.log("I got a notification", JSON.stringify(lastnotification));
+  }, [lastnotification]);
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
+      {lastnotification && (
+        <Text>{JSON.stringify(lastnotification, null, 2)}</Text>
+      )}
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +38,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
